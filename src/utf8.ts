aa133c898e676ec22a803c0108f3cc3917deb524
removedporn <86824510+removedporn@users.noreply.github.com>
2022-08-19 15:13:45 +0800

    Delete src directory

--- src/utf8.ts
+++
@@ -1,6 +0,0 @@
-declare const require: any;
-
-type ctor<T> = new() => T;
-
-export const Decoder: ctor<TextDecoder> = (typeof TextDecoder === 'undefined') ? require('util').TextDecoder : TextDecoder;
-export const Encoder: ctor<TextEncoder> = (typeof TextEncoder=== 'undefined') ? require('util').TextEncoder : TextEncoder;
